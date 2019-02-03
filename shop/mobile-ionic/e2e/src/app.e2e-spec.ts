4ed5ad9df72e1497d6ec8984fa5b6f2022650afb
Ruslan Konviser <evereq@gmail.com>
2019-02-03 03:41:48 +0200

    feat: open source mobile-ionic shop (partially)

---
+++ shop/mobile-ionic/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,15 @@
+import 'jasmine';
+import { AppPage } from './app.po';
+
+describe('new App', () => {
+	let page: AppPage;
+
+	beforeEach(() => {
+		page = new AppPage();
+	});
+
+	it('should display welcome message', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toContain('The world is your oyster.');
+	});
+});
