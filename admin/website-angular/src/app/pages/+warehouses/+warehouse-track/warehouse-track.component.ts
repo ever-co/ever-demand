926046552a7207367fa28e0a39ed1256a947ce3e
Emil Momchilov <emil@pernik.org>
2019-04-19 17:06:53 +0300

    feat(ever-admin): added new page and track button

---
+++ admin/website-angular/src/app/pages/+warehouses/+warehouse-track/warehouse-track.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	templateUrl: './warehouse-track.component.html',
+	styleUrls: ['./warehouse-track.component.scss']
+})
+export class WarehouseTrackComponent {
+	constructor() {}
+}
