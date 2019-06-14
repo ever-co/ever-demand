83a5867050e532f9bbaf5619e94f09405bbafc78
K-Kit <kylekitlinski@gmail.com>
2019-06-14 14:37:34 -0400

    more order update

--- shop/gatsby-starter-ever/src/apollo/order.tsx
+++ shop/gatsby-starter-ever/src/apollo/order.tsx
@@ -14,6 +14,7 @@ query fetchOrders($id: String!){
 			updatedAt
 			orderNumber
 			status
+			totalPrice
           products {
             isTakeaway
             product {
--- shop/gatsby-starter-ever/src/pages/orders.tsx
+++ shop/gatsby-starter-ever/src/pages/orders.tsx
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import styles from './orders.module.scss'
 import { useQuery } from 'react-apollo-hooks';
 import { getOrders } from '../apollo/order';
-import { Card } from '@material-ui/core';
+import {Button, Card, Container, Grid, Typography} from '@material-ui/core';
 import { AppContext } from '../Context';
 import Layout from '../components/layout';
 import OrdersList from '../components/OrdersList/OrdersList';
+import {navigate} from "gatsby";
 
 
 const Orders = (props) => {
 	const context = useContext(AppContext)
 	// @ts-ignore
 	const { data, error, loading } = useQuery(getOrders, {variables:{id: context.getUser().id}})
-	console.log(data)
 	const orders = data.getOrders;
 
 	if (loading) {
@@ -23,10 +23,31 @@ const Orders = (props) => {
 		// @ts-ignore
 		return <div>Error! {error.message}</div>;
 	}
+	const sum = (_total, order) => {
+		return _total + order;
+	}
+	const totals = orders.map(order => order.totalPrice)
+	const total = totals.reduce(sum, 0)
+	console.log(total)
 	return (
 		<Layout>
-			hi
-			<OrdersList orders={orders}/>
+			<Container maxWidth={'xl'}>
+				<Grid container justify={'center'} spacing={4}>
+					<Grid item xs={12} lg={'auto'} >
+						<Button variant={'contained'}
+								color={'secondary'}
+						>
+							Pay Now: ${total}
+						</Button>
+						<br />
+						<Typography variant={'body1'}> Or pay with cash </Typography>
+					</Grid>
+
+					<Grid item xs={12} lg={8} xl={10}>
+						<OrdersList orders={orders}/>
+					</Grid>
+				</Grid>
+			</Container>
 		</Layout>
 	)
 };
