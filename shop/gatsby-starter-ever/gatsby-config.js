4a96a49eca0780e9827342e0414fd81cbe923f60
K-Kit <kylekitlinski@gmail.com>
2019-06-17 18:38:50 -0400

    gp updates

--- shop/gatsby-starter-ever/gatsby-config.js
+++ shop/gatsby-starter-ever/gatsby-config.js
@@ -6,6 +6,19 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+	  {
+		  resolve: `gatsby-plugin-material-ui`,
+		  // If you want to use styled components, in conjunction to Material-UI, you should:
+		  // - Change the injection order
+		  // - Add the plugin
+		  options: {
+			  stylesProvider: {
+			    injectFirst: true,
+			  },
+		  },
+	  },
+
+  'gatsby-plugin-styled-components',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
