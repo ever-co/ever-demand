bf6d4ae28cc0e0cee22cea8662946ee31a702b71
K-Kit <kylekitlinski@gmail.com>
2019-06-17 13:16:58 -0400

    Update Header.tsx

--- shop/gatsby-starter-ever/src/pages/login/Header.tsx
+++ shop/gatsby-starter-ever/src/pages/login/Header.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './login.module.scss';
+import {Grid} from '@material-ui/core';
 // import Map from '../Map'
 
 const Header = ({ title, subtitle, logo }) => {
 	return (
-		<div className={styles.content + styles.auth}>
-			<div className={styles.formContent}>
-				<div className={styles.logo}>
+		<Grid container justify={'center'} className={styles.content + styles.auth} direction={'column'}>
+			<Grid item className={styles.formContent}>
 					<img src={logo} />
-				</div>
+			</Grid>
+			<Grid item>
 				<h2 className={styles.title}>{title}</h2>
+			</Grid>
+			<Grid item>
 				<h2 className={styles.slogan + styles.locationSlogan}>{subtitle}</h2>
-			</div>
-		</div>
+			</Grid>
+		</Grid>
 	);
 };
 
