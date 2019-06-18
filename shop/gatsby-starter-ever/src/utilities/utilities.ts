e80b1d56d77ffb9f2560b3726e8c4ed948776719
K-Kit <kylekitlinski@gmail.com>
2019-06-18 14:41:13 -0400

    update utils

--- shop/gatsby-starter-ever/src/utilities/utilities.ts
+++ shop/gatsby-starter-ever/src/utilities/utilities.ts
@@ -1,16 +1,4 @@
 import { getlocale } from '../auth';
-/**
- * Formats a Vendure price (an integer) into a localised dollar-cents format.
- * @param currency
- * @param value
- */
-export function formatPrice(currency: string, value: number): string {
-	return new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency,
-		currencyDisplay: 'symbol',
-	}).format(value / 100);
-}
 
 export const matchLocale = arr =>
 	arr.filter(item => item.locale === getlocale()).value || arr[0].value;
