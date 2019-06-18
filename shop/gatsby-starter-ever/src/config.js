cdfb3bd55d246e4caaeca98e7b474df74f711874
K-Kit <kylekitlinski@gmail.com>
2019-06-18 15:00:51 -0400

    update config

--- shop/gatsby-starter-ever/src/config.js
+++ shop/gatsby-starter-ever/src/config.js
@@ -1,6 +1,6 @@
 // todo move gmaps api to .env
 module.exports = {
-	graphqlUri: 'http://45.77.216.107:5555/graphql',
+	graphqlUri: 'http://localhost:5555/graphql',
 	defaultCoordinates: [23.3832736, 42.5659136],
-	GOOGLE_MAPS_API_KEY: 'AIzaSyBqF2OEwTZL_gAOMn0mx8QqgajtzsrO-VQ',
+	GOOGLE_MAPS_API_KEY: '',
 };
