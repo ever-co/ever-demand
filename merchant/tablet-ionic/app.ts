7b3e8c435b44aed5e5f39e7629f35f81b460f6a4
alish <alish931@abv.bg>
2020-01-28 18:37:13 +0200

    feat: improved merchant app build and routing

--- merchant/tablet-ionic/app.ts
+++ merchant/tablet-ionic/app.ts
@@ -6,6 +6,6 @@ import { env } from './scripts/env';
 const port: number = env.PORT;
 
 connect()
-	.use(serveStatic(path.join(__dirname, 'www')))
+	.use(serveStatic(path.join(__dirname, '../')))
 	.listen(port);
 console.log(`listening on ${port}`);
