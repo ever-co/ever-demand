2c36b9e9d372e9f1ffb0a188fe8fdcdd49b109b1
YavorGrancharov <bearhold@gmail.com>
2020-06-02 16:14:39 +0300

    fix: removed some unused imports

--- packages/admin-web-angular/src/app/pages/+carriers/+carrier/carrier-orders/carrier-orders.component.ts
+++ packages/admin-web-angular/src/app/pages/+carriers/+carrier/carrier-orders/carrier-orders.component.ts
@@ -19,14 +19,13 @@ import _ from 'lodash';
 import { LocalDataSource } from 'ng2-smart-table';
 import { Subject, Observable } from 'rxjs/Rx';
 import { TranslateService } from '@ngx-translate/core';
-import { forkJoin, Subscription, of } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 import { CreatedComponent } from '../../../../@shared/render-component/created/created.component';
 import { CarriersService } from '@app/@core/data/carriers.service';
 import { GeoLocationOrdersService } from '@app/@core/data/geo-location-orders.service';
 import GeoLocation from '@modules/server.common/entities/GeoLocation';
 import { StoreOrderComponent } from '@app/@shared/render-component/carrier-orders-table/store-order.component';
 import { UserOrderComponent } from '@app/@shared/render-component/carrier-orders-table/user-order-component';
-import { takeUntil, catchError } from 'rxjs/operators';
 
 const perPage = 3;
 let searchCustomer: boolean;
