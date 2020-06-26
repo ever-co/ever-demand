bfe7ff0144ceead037a70d9520b69d0eab347576
Yavor Grancharov <bearhold@gmail.com>
2020-06-26 11:22:55 +0300

    fix: store save btn disabled on valid changes

--- packages/admin-web-angular/src/app/@shared/warehouse/forms/warehouse-manage-tabs/warehouse-manage-tabs.component.ts
+++ packages/admin-web-angular/src/app/@shared/warehouse/forms/warehouse-manage-tabs/warehouse-manage-tabs.component.ts
@@ -85,11 +85,7 @@ export class WarehouseManageTabsComponent {
 	}
 
 	get validForm() {
-		return (
-			this.form.valid &&
-			this.contactInfoForm.validForm &&
-			this.paymentsSettingsForm.isPaymentValid
-		);
+		return this.form.valid;
 	}
 
 	get deliveryAreas() {
