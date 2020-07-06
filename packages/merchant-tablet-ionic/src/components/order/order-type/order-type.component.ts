82b9f8ab45aee27fce9e343b693b518aeacefc66
Yavor Grancharov <bearhold@gmail.com>
2020-07-06 17:14:04 +0300

    fix: removed unused import

--- packages/merchant-tablet-ionic/src/components/order/order-type/order-type.component.ts
+++ packages/merchant-tablet-ionic/src/components/order/order-type/order-type.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import DeliveryType from '@modules/server.common/enums/DeliveryType';
 
 @Component({
