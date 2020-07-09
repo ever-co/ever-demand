61ccc20ac19d4e272cbbe643aabf4404cbb33126
Valentin <valiopld@gmail.com>
2020-07-09 11:04:12 +0300

    fix: orders and status not updating immediately

--- packages/shop-web-angular/src/app/+orders/orders.container.component.ts
+++ packages/shop-web-angular/src/app/+orders/orders.container.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { UserOrdersRouter } from '@modules/client.common.angular2/routers/user-orders-router.service';
 import Order from '@modules/server.common/entities/Order';
 import { Store } from 'app/services/store';
 
 @Component({
 	selector: 'orders-container',
-	template: '<orders *ngIf="orders " [orders]="orders"></orders>',
+	template: `
+		<orders *ngIf="orders" [orders]="orders"></orders>
+		<div
+			*ngIf="!orders.length"
+			style="text-align:center; font-size:28px;margin:20px 0"
+		>
+			There is no orders ...
+		</div>
+	`,
 })
 export class OrdersContainerComponent {
 	// TODO: add correct type of orders variable!
@@ -15,13 +23,15 @@ export class OrdersContainerComponent {
 
 	constructor(
 		private readonly userOrdersRouter: UserOrdersRouter,
+		private readonly ngZone: NgZone,
 		private readonly store: Store
 	) {
 		const userId = store.userId;
 		// During testing: this.userOrdersRouter.getOrderedProducts('23');
 		this.userOrdersRouter.get(userId).subscribe((res) => {
-			this.orders = res.filter((r) => !r.isCancelled);
-			res = res.filter((r) => !r.isCancelled);
+			this.ngZone.run(() => {
+				this.orders = res.filter((r) => !r.isCancelled);
+			});
 		});
 	}
 }
