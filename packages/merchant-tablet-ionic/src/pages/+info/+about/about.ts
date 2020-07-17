b74848618fa9be077d7935d9482cae865d5dd186
sunko <sunko@ever.co>
2020-07-17 12:49:59 +0300

    feat: implemented about us in merchant app

--- packages/merchant-tablet-ionic/src/pages/+info/+about/about.ts
+++ packages/merchant-tablet-ionic/src/pages/+info/+about/about.ts
@@ -1,46 +1,28 @@
 import { Component, OnDestroy } from '@angular/core';
 import { UserRouter } from '@modules/client.common.angular2/routers/user-router.service';
-import { Store } from 'services/store.service';
-import { environment } from 'environments/environment';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'page-about',
 	templateUrl: 'about.html',
 })
 export class AboutPage implements OnDestroy {
-	public aboutHtml: string;
-
-	private s$: any;
-
-	public appVersion: string;
-
-	constructor(private userRouter: UserRouter, private store: Store) {
-		this._getAboutHtml();
-		this.appVersion = environment.APP_VERSION;
+	public useAboutHtml: string = '<h1>Loading...</h1>';
+	public selectedLanguage: string;
+	private sub: Subscription;
+	constructor(private userRouter: UserRouter) {
+		this.selectedLanguage = localStorage.getItem('_language');
 	}
 
-	get userId() {
-		return localStorage.getItem('_userId');
+	ngOnInit() {
+		this.sub = this.userRouter
+			.getAboutUsByLanguage(this.selectedLanguage)
+			.subscribe((html) => {
+				this.useAboutHtml = html;
+			});
 	}
 
-	get deviceId() {
-		return localStorage.getItem('_deviceId');
-	}
-
-	private _getAboutHtml() {
-		const deviceId = this.deviceId;
-		if (this.userId && deviceId) {
-			this.s$ = this.userRouter
-				.getAboutUs(this.userId, deviceId)
-				.subscribe((html) => {
-					this.aboutHtml = html;
-				});
-		}
-	}
-
-	ngOnDestroy(): void {
-		if (this.s$) {
-			this.s$.unsubscribe();
-		}
+	ngOnDestroy() {
+		this.sub.unsubscribe();
 	}
 }
