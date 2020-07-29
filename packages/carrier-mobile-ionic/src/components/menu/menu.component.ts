5278a5668c3e22e43d7a8dc977416af45e5a3bac
Yavor Grancharov <bearhold@gmail.com>
2020-07-29 12:15:27 +0300

    fix: change app direction on hebrew

--- packages/carrier-mobile-ionic/src/components/menu/menu.component.ts
+++ packages/carrier-mobile-ionic/src/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from 'services/store.service';
 import { environment } from 'environments/environment';
+import { Platform } from '@ionic/angular';
 
 @Component({
 	selector: 'e-cu-menu',
@@ -10,7 +11,7 @@ import { environment } from 'environments/environment';
 export class MenuComponent {
 	companyName: string;
 
-	constructor(private store: Store) {
+	constructor(private store: Store, public platform: Platform) {
 		this.companyName = environment.APP_NAME;
 	}
 
