ce8fb7c8f1fd1560d85b2ddc204248f36eb4f1a8
Ruslan Konviser <evereq@gmail.com>
2021-10-25 12:35:37 +0300

    fix: build for Server (still broken for now)

--- packages/common/src/@pyro/db/db-object.ts
+++ packages/common/src/@pyro/db/db-object.ts
@@ -14,7 +14,8 @@ export interface DBObjectClass extends Function {
 export abstract class DBObject<
 	RawObject extends CreateObject & DBRawObject,
 	CreateObject extends DBCreateObject
-> implements DBRawObject {
+> implements DBRawObject
+{
 	// to allow inferring the generic args types
 	readonly CreateObjectTYPE: CreateObject;
 
