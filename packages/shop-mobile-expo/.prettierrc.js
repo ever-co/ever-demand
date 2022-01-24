79b338a039e2d9d591a7cda0204066e8ecd4e022
Neosoulink <neodarksoulink@gmail.com>
2022-01-24 10:33:59 +0100

    refactor: update files extensions

--- packages/shop-mobile-expo/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  useTabs: true,
-};
---
+++ packages/shop-mobile-expo/.prettierrc.ts
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	useTabs: true,
+};
