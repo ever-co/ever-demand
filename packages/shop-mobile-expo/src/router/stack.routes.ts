7f078cd0ed18b30780846c2153d35050694db5e2
Neosoulink <neodarksoulink@gmail.com>
2022-01-27 17:10:56 +0100

    refactor: stack routes logic

---
+++ packages/shop-mobile-expo/src/router/stack.routes.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// TYPES
+import { GroupNameType } from './groups.routes';
+
+// UTILS
+import { getReactComponentProps } from '../helpers/utils';
+
+// SCREENS
+import SCREENS from '../screens/index';
+
+const StackScreen = createNativeStackNavigator().Screen;
+const StackScreenProps = getReactComponentProps(StackScreen);
+
+// LOCAL TYPES
+export type ScreenType = typeof StackScreenProps;
+export type ScreenGroupType = {
+	[name in GroupNameType]: ScreenType[];
+};
+
+const ROUTES_GROUPS: ScreenGroupType = {
+	APP: [],
+	BLANK: [
+		{
+			name: 'STACK/BLANK_',
+			component: SCREENS.Blank_,
+		},
+	],
+	LOADING: [
+		{
+			name: 'STACK/LOADING',
+			component: SCREENS.Loading,
+		},
+	],
+};
+
+export default ROUTES_GROUPS;
