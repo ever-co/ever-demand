6e54705ae335b4580d9504b3ca340fc2528a67ed
Neosoulink <neodarksoulink@gmail.com>
2022-01-28 17:51:57 +0100

    refactor: update types using json languages

--- packages/shop-mobile-expo/src/store/features/translation/types.ts
+++ packages/shop-mobile-expo/src/store/features/translation/types.ts
@@ -1,7 +1,13 @@
+// LANGUAGES
+import LANGUAGES from "./lang";
+
 // TYPES
-export type supportedLangType = 'bg-BG' | 'en-US' | 'es-ES' | 'he-IL' | 'ru-RU';
+export type supportedLangType = "bg_BG" | "en_US" | "es_ES" | "he_IL" | "ru_RU";
 export type supportedLangsType = {
 	readonly [name in supportedLangType]: supportedLangType;
+};
+export type supportedLangsObjectType = {
+	readonly [name in supportedLangType]: typeof LANGUAGES;
 };
 export type TranslationStateType = {
 	lang: supportedLangType;
