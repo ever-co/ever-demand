9e622c81406e6712b81fd26058033fe6e69974e8
Neosoulink <neodarksoulink@gmail.com>
2022-01-28 17:53:10 +0100

    refactor: updates data that depends to updated types

--- packages/shop-mobile-expo/src/store/features/translation/index.ts
+++ packages/shop-mobile-expo/src/store/features/translation/index.ts
@@ -1,28 +1,28 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// LANGUAGES
+import LANGUAGES from "./lang";
 
 // TYPES
 import {
 	supportedLangType,
 	supportedLangsType,
 	TranslationStateType,
-} from './types';
-import type { RootState } from '../../index';
-
-// CONSTANTS
+} from "./types";
+import type { RootState } from "../../index";
 
 export const supportedLangs: supportedLangsType = {
-	['bg-BG']: 'bg-BG',
-	['en-US']: 'en-US',
-	['es-ES']: 'es-ES',
-	['he-IL']: 'he-IL',
-	['ru-RU']: 'ru-RU',
+	bg_BG: "bg_BG",
+	en_US: "en_US",
+	es_ES: "es_ES",
+	he_IL: "he_IL",
+	ru_RU: "ru_RU",
 };
 export const initialState: TranslationStateType = {
-	lang: supportedLangs['en-US'],
+	lang: supportedLangs.en_US,
 };
-
 export const translationSlice = createSlice({
-	name: 'translation',
+	name: "translation",
 	initialState,
 	reducers: {
 		setLang: (state, action: PayloadAction<supportedLangType>) => {
@@ -37,6 +37,5 @@ export const { setLang } = translationSlice.actions;
 // SELECTORS
 export const getLang = (state: RootState) => state.translation.lang;
 export const getLanguage = (state: RootState) =>
-	require(`../../../assets/i18n/${state.translation.lang}.json`);
-
+	LANGUAGES[state.translation.lang];
 export default translationSlice.reducer;
