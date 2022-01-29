07a3265628551df57e97cbb4858288f28d2b3faa
Neosoulink <neodarksoulink@gmail.com>
2022-01-29 18:23:25 +0100

    feat: design drawer header

--- packages/shop-mobile-expo/src/components/CustomDrawerContent/Header.tsx
+++ packages/shop-mobile-expo/src/components/CustomDrawerContent/Header.tsx
@@ -1,11 +1,40 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// COMPONENTS
+import PaperText from "../PaperText";
+
+// STYLES
+import {
+	GLOBAL_STYLE as GS,
+	CONSTANT_SIZE as CS,
+	CONSTANT_COLOR as CC,
+} from "../../assets/ts/styles";
 
 const DrawerHeader = () => {
 	return (
-		<View>
-			<Text>Drawer Header</Text>
-		</View>
+		<SafeAreaView
+			style={{
+				...GS.bgSecondary,
+				...GS.shadow,
+				borderBottomStartRadius: CS.SPACE,
+				borderBottomEndRadius: CS.SPACE,
+			}}
+		>
+			<View
+				style={{
+					...GS.centered,
+					height: CS.DRAWER_HEADER_HEIGHT,
+				}}
+			>
+				<PaperText
+					style={{ ...GS.fontBold, color: CC.light, fontSize: CS.FONT_SIZE_MD }}
+				>
+					Ever Shop
+				</PaperText>
+			</View>
+		</SafeAreaView>
 	);
 };
 
