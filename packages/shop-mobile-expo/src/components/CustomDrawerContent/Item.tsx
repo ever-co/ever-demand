554419e03d549dc553316f9c665648fd1213ae11
Neosoulink <neodarksoulink@gmail.com>
2022-01-29 22:33:15 +0100

    refactor: design custom drawer items

--- packages/shop-mobile-expo/src/components/CustomDrawerContent/Item.tsx
+++ packages/shop-mobile-expo/src/components/CustomDrawerContent/Item.tsx
@@ -1,38 +1,45 @@
 import * as React from "react";
+import { View } from "react-native";
 import { DrawerItem } from "@react-navigation/drawer";
 
 // TYPES
 import { DrawerLinkItem } from "../../router/drawer.routes";
 
 // COMPONENTS
-import TouchableCard, { TouchableCardPropsType } from "../TouchableCard";
+import TouchableCard from "../TouchableCard";
 import Icon from "../Icon";
+import PaperText from "../PaperText";
 
 // STYLES
-import { GLOBAL_STYLE as GS } from "../../assets/ts/styles";
+import {
+	GLOBAL_STYLE as GS,
+	CONSTANT_COLOR as CC,
+	CONSTANT_SIZE as CS,
+} from "../../assets/ts/styles";
 
 const Item: React.FC<DrawerLinkItem> = ({ label, path, icon, external }) => {
 	return (
-		<DrawerItem
-			icon={({ focused, color, size }) =>
-				icon ? (
-					<Icon
-						color={color}
-						size={size}
-						name={
-							icon
+		<TouchableCard
+			style={{ ...GS.w100, ...GS.inlineItems, ...GS.mb1 }}
+			cardStyle={{ ...GS.w100, ...GS.px0, borderRadius: 0 }}
+			cardStyleContent={{ ...GS.px2 }}
+			height={50}
+			rippleColor={CC.secondaryHighLight + "3f"}
+			iconProps={
+				icon
+					? {
+							color: CC.primaryHightLight,
+							size: CS.FONT_SIZE * 1.5,
+							name: icon,
 							// TODO: think to use this feature (below e.g)
 							//focused ? 'heart' : 'heart-outline'
-						}
-					/>
-				) : (
-					icon
-				)
+					  }
+					: undefined
 			}
-			label={label}
-			style={{ ...GS.mx0 }}
-			onPress={function (): void {
-				console.warn("Function not implemented.");
+			title={label}
+			titleStyle={{ color: CC.primaryLight, fontSize: CS.FONT_SIZE }}
+			onPress={() => {
+				return console.log("Function not implemented.");
 			}}
 		/>
 	);
