1218373795c184983dafa4f9075d0cd5bf29922e
Neosoulink <neodarksoulink@gmail.com>
2022-01-29 17:44:42 +0100

    refactor: update custom drawer content name

---
+++ packages/shop-mobile-expo/src/components/CustomDrawerContent/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const DrawerHeader = () => {
+	return (
+		<View>
+			<Text>Drawer Header</Text>
+		</View>
+	);
+};
+
+export default DrawerHeader;
