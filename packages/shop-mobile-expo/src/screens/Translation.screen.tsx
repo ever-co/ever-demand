2a447e4d373921d4255bf5ffa83a9b7f09220ba9
Neosoulink <neodarksoulink@gmail.com>
2022-01-30 17:27:08 +0100

    feat: set translations feature

--- packages/shop-mobile-expo/src/screens/Translation.screen.tsx
+++ packages/shop-mobile-expo/src/screens/Translation.screen.tsx
@@ -1,22 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { RadioButton } from "react-native-paper";
 
-// SELECTORS
-import { useAppSelector } from "../store/hooks";
-import { getLanguage } from "../store/features/translation";
+// TYPES
+import type { supportedLangType } from "../store/features/translation/types";
+
+// ACTIONS & SELECTORS
+import { useAppSelector, useAppDispatch } from "../store/hooks";
+import {
+	getLang,
+	getLanguage,
+	supportedLangs,
+	setLang,
+} from "../store/features/translation";
 
 // COMPONENTS
+import TouchableCard from "../components/TouchableCard";
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
 import PaperText from "../components/PaperText";
-
-// STYLES
-import { GLOBAL_STYLE as GS, CONSTANT_COLOR as CC } from "../assets/ts/styles";
-import { ScrollView } from "react-native-gesture-handler";
 import CustomScreenHeader from "../components/CustomScreenHeader";
 
+// STYLES
+import {
+	GLOBAL_STYLE as GS,
+	CONSTANT_COLOR as CC,
+	CONSTANT_SIZE as CS,
+} from "../assets/ts/styles";
+
 function HomeScreen({}) {
+	// ACTIONS
+	const translate = useAppDispatch();
+
 	// SELECTORS
-	const LANGUAGE = useAppSelector(getLanguage);
+	const currentLang = useAppSelector(getLang);
+	const languages = useAppSelector(getLanguage);
 
 	return (
 		<View style={{ ...GS.screen }}>
@@ -25,13 +42,48 @@ function HomeScreen({}) {
 				backgroundColor="transparent"
 				barStyle="light-content"
 			/>
-			<CustomScreenHeader title={LANGUAGE.LANGUAGE_VIEW.TITLE} showHomeBtn />
+			<CustomScreenHeader title={languages.LANGUAGE_VIEW.TITLE} showHomeBtn />
 
 			<ScrollView
-				style={{ ...GS.h100, ...GS.pt4, ...GS.px2, ...GS.bgLight }}
+				style={{
+					...GS.h100,
+					...GS.pt4,
+					...GS.px2,
+					...GS.bgLight,
+				}}
 				scrollEnabled
 			>
-				<PaperText style={{ color: CC.primary }}>Lang 1</PaperText>
+				{Object.keys(supportedLangs).map((lang: string) => {
+					let L = lang as supportedLangType;
+
+					return (
+						<TouchableCard
+							style={{ ...GS.inlineItems, ...GS.mb2 }}
+							cardStyle={{ ...GS.w100, borderRadius: 5 }}
+							cardStyleContent={{ borderRadius: 0 }}
+							height={CS.FONT_SIZE_MD * 3}
+							onPress={() => translate(setLang(L))}
+						>
+							<View style={{ ...GS.justifyContentBetween, ...GS.w100 }}>
+								<PaperText
+									style={{ color: CC.primary, fontSize: CS.FONT_SIZE_MD }}
+								>
+									{
+										// @ts-ignore
+										languages[lang]
+									}
+								</PaperText>
+
+								<RadioButton
+									status={lang === currentLang ? "checked" : "unchecked"}
+									uncheckedColor={CC.primaryHightLight}
+									value={lang}
+									onPress={() => translate(setLang(L))}
+								/>
+							</View>
+						</TouchableCard>
+					);
+				})}
 			</ScrollView>
 		</View>
 	);
