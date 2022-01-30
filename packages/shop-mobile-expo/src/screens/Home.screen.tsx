d700919d9210cc4223f40e4a60bb50f822857349
Neosoulink <neodarksoulink@gmail.com>
2022-01-30 13:40:01 +0100

    feat: implement custom header

--- packages/shop-mobile-expo/src/screens/Home.screen.tsx
+++ packages/shop-mobile-expo/src/screens/Home.screen.tsx
@@ -8,19 +8,25 @@ import PaperText from "../components/PaperText";
 
 // STYLES
 import { GLOBAL_STYLE as GS } from "../assets/ts/styles";
+import { ScrollView } from "react-native-gesture-handler";
+import CustomScreenHeader from "../components/CustomScreenHeader";
 
 function HomeScreen({}) {
 	return (
-		<View style={{ ...GS.screen, ...GS.centered }}>
+		<View style={{ ...GS.screen }}>
 			<FocusAwareStatusBar
 				translucent={true}
 				backgroundColor="transparent"
 				barStyle="light-content"
 			/>
-			<PaperText>Welcome to your new app build with</PaperText>
-			<PaperText>
-				Typescript / Expo / Paper / React Navigation / Redux
-			</PaperText>
+			<CustomScreenHeader title="Home" showControls />
+
+			<ScrollView style={{ ...GS.h100, ...GS.pt3 }}>
+				<PaperText>Welcome to your new app build with</PaperText>
+				<PaperText>
+					Typescript / Expo / Paper / React Navigation / Redux
+				</PaperText>
+			</ScrollView>
 		</View>
 	);
 }
