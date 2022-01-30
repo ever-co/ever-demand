304abe552e377998a7a9ba4534a6610523a01123
Neosoulink <neodarksoulink@gmail.com>
2022-01-30 13:59:47 +0100

    feat: add translation

--- packages/shop-mobile-expo/src/screens/Home.screen.tsx
+++ packages/shop-mobile-expo/src/screens/Home.screen.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+// SELECTORS
+import { useAppSelector } from "../store/hooks";
+import { getLanguage } from "../store/features/translation";
 
 // COMPONENTS
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
@@ -12,6 +14,9 @@ import { ScrollView } from "react-native-gesture-handler";
 import CustomScreenHeader from "../components/CustomScreenHeader";
 
 function HomeScreen({}) {
+	// SELECTORS
+	const LANGUAGE = useAppSelector(getLanguage);
+
 	return (
 		<View style={{ ...GS.screen }}>
 			<FocusAwareStatusBar
@@ -19,13 +24,10 @@ function HomeScreen({}) {
 				backgroundColor="transparent"
 				barStyle="light-content"
 			/>
-			<CustomScreenHeader title="Home" showControls />
+			<CustomScreenHeader title={LANGUAGE.PRODUCTS_VIEW.TITLE} showControls />
 
 			<ScrollView style={{ ...GS.h100, ...GS.pt3 }}>
-				<PaperText>Welcome to your new app build with</PaperText>
-				<PaperText>
-					Typescript / Expo / Paper / React Navigation / Redux
-				</PaperText>
+				<PaperText>No data</PaperText>
 			</ScrollView>
 		</View>
 	);
