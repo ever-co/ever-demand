0b66144bbc9dff428cca31dc02f5e4944fa8ae05
Neosoulink <neodarksoulink@gmail.com>
2022-01-30 09:36:57 +0100

    feat: async load fonts

--- packages/shop-mobile-expo/src/App.tsx
+++ packages/shop-mobile-expo/src/App.tsx
@@ -1,9 +1,11 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { Provider } from "react-redux";
-import Icon from "@expo/vector-icons/AntDesign";
 import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import { setCustomTextInput, setCustomText } from "react-native-global-props";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+import Icon from "@expo/vector-icons/AntDesign";
 
 // STORE
 import { store } from "./store";
@@ -32,18 +34,40 @@ const theme: ThemeType = {
 		placeholder: CC.grayLight,
 		error: CC.danger,
 	},
+	fonts: {
+		thin: GS.FF_NunitoExtraLight,
+		light: GS.FF_NunitoLight,
+		regular: GS.FF_Nunito,
+		medium: GS.FF_NunitoSemiBold,
+	},
 };
 
 setCustomTextInput({
-	style: {},
+	style: {
+		...GS.FF_Nunito,
+	},
 });
 
 setCustomText({
-	style: {},
+	style: {
+		...GS.FF_Nunito,
+	},
 });
 
 export default function App() {
-	return (
+	const [fontsLoaded] = useFonts({
+		"Nunito-ExtraLight": require("./assets/fonts/Nunito/Nunito-ExtraLight.ttf"),
+		"Nunito-Light": require("./assets/fonts/Nunito/Nunito-Light.ttf"),
+		"Nunito-Regular": require("./assets/fonts/Nunito/Nunito-Regular.ttf"),
+		"Nunito-SemiBold": require("./assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+		"Nunito-Bold": require("./assets/fonts/Nunito/Nunito-Bold.ttf"),
+		"Nunito-Black": require("./assets/fonts/Nunito/Nunito-Black.ttf"),
+		"Lobster-Regular": require("./assets/fonts/Lobster/Lobster-Regular.ttf"),
+	});
+
+	return !fontsLoaded ? (
+		<AppLoading />
+	) : (
 		<Provider store={store}>
 			<PaperProvider
 				theme={theme}
