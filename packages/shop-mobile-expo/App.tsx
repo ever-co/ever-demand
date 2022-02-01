ee431b7a4cb240e9ac2589a403b1cb12d466f5bd
Neosoulink <neodarksoulink@gmail.com>
2022-02-01 20:01:32 +0100

    refactor: import registerRootComponent from expo

--- packages/shop-mobile-expo/App.tsx
+++ packages/shop-mobile-expo/App.tsx
@@ -1,6 +1,9 @@
-import registerRootComponent from "expo/build/launch/registerRootComponent";
+import { registerRootComponent } from 'expo';
 
 // APP COMPONENT
-import App from "./src/App";
+import App from './src/App';
 
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
 registerRootComponent(App);
