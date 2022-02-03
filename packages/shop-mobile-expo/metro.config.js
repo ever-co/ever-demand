41c73e4c4bc48e5a7f3be963943224fc74a7445a
Neosoulink <neodarksoulink@gmail.com>
2022-02-03 14:12:45 +0100

    fix: resole cjs files in metro builder

--- packages/shop-mobile-expo/metro.config.js
+++ packages/shop-mobile-expo/metro.config.js
@@ -7,16 +7,16 @@ const workspaceRoot = path.resolve(__dirname, "../..");
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
-
 // 1. Watch all files within the monorepo
 //config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages, and in what order
+config.resolver.sourceExts.push("cjs");
 config.resolver.nodeModulesPaths = [
 	path.resolve(projectRoot, "node_modules"),
 	path.resolve(workspaceRoot, "node_modules"),
 ];
 
-console.log("workspaceRoot =====>", workspaceRoot);
+console.log("Expo config =====>", config);
 
 module.exports = config;
 
