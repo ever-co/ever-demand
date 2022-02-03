bb4b728bf5054e97e612d5e9b5b3b0363f137079
Neosoulink <neodarksoulink@gmail.com>
2022-02-03 10:34:46 +0100

    fix: temporary fix to run app in separete project folder

--- packages/shop-mobile-expo/metro.config.js
+++ packages/shop-mobile-expo/metro.config.js
@@ -1,19 +1,25 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, '../..');
+const workspaceRoot = path.resolve(__dirname, "../..");
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+//config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, 'node_modules'),
-	path.resolve(workspaceRoot, 'node_modules'),
+	path.resolve(projectRoot, "node_modules"),
+	path.resolve(workspaceRoot, "node_modules"),
 ];
 
+console.log("workspaceRoot =====>", workspaceRoot);
+
 module.exports = config;
+
+//const { createMetroConfiguration } = require('expo-yarn-workspaces');
+
+//module.exports = createMetroConfiguration(__dirname);
