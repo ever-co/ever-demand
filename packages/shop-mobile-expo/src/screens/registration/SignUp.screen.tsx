90f682e5fb1dc9407c09209b25df4bb81ed4b133
Neosoulink <neodarksoulink@gmail.com>
2022-02-03 16:58:13 +0100

    feat: minor design applayed

--- packages/shop-mobile-expo/src/screens/registration/SignUp.screen.tsx
+++ packages/shop-mobile-expo/src/screens/registration/SignUp.screen.tsx
@@ -1,20 +1,56 @@
 import React from "react";
-import { Text } from "react-native";
-
+import { Text, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+// SELECTORS
+import { useAppSelector } from "../../store/hooks";
+import { getLanguage } from "../../store/features/translation";
+
 // COMPONENTS
-import FocusAwareStatusBar from "../../components/FocusAwareStatusBar";
+import { FocusAwareStatusBar } from "../../components/Common";
+
+// STYLES
+import { GLOBAL_STYLE as GS } from "../../assets/ts/styles";
 
 const SignUpScreen = () => {
+	// SELECTORS
+	const getCurrentLanguage = useAppSelector(getLanguage);
+
 	return (
-		<SafeAreaView>
+		<SafeAreaView style={{ ...GS.screen }}>
 			<FocusAwareStatusBar
 				translucent={true}
 				backgroundColor="transparent"
 				barStyle="light-content"
 			/>
-			<Text>Sign Up</Text>
+
+			<View
+				style={{
+					...GS.screen,
+					...GS.bgTransparent,
+					zIndex: 1,
+				}}
+			>
+				{/*  Title logo */}
+				<View
+					style={{
+						...GS.centered,
+						flex: 1,
+					}}
+				>
+					<Image
+						source={require("../../assets/img/ever/logo.png")}
+						resizeMode="contain"
+						style={{ ...GS.w100, height: 100, marginBottom: -10 }}
+					/>
+					<Text style={{ ...GS.txtCapitalize }}>
+						{getCurrentLanguage.INVITE_VIEW.BY_CODE.LOGO.DETAILS}
+					</Text>
+				</View>
+
+				{/* Social Networks buttons */}
+				<View style={{ ...GS.py4 }}></View>
+			</View>
 		</SafeAreaView>
 	);
 };
