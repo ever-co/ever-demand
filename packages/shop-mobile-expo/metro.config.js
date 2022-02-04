c01a4ed131f80b166d94bb3506cab252baa15c63
Neosoulink <neodarksoulink@gmail.com>
2022-02-04 13:59:27 +0100

    style: format code for following linters rules

--- packages/shop-mobile-expo/metro.config.js
+++ packages/shop-mobile-expo/metro.config.js
@@ -1,22 +1,22 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, "../..");
+const workspaceRoot = path.resolve(__dirname, '../..');
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
-// 1. Watch all files within the monorepo
-//config.watchFolders = [workspaceRoot];
+// 1. Watch all files within the monorepo (require watchman installed! disabled for now)
+// config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages, and in what order
-config.resolver.sourceExts.push("cjs");
+config.resolver.sourceExts.push('cjs');
 config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, "node_modules"),
-	path.resolve(workspaceRoot, "node_modules"),
+	path.resolve(projectRoot, 'node_modules'),
+	path.resolve(workspaceRoot, 'node_modules'),
 ];
 
-console.log("Expo config =====>", config);
+console.log('Expo config =====>', config);
 
 module.exports = config;
 
