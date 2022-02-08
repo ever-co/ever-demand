8968e7f420e4c530c4cc1516d42df35d61cce744
Neosoulink <neodarksoulink@gmail.com>
2022-02-08 12:16:38 +0100

    style: reordered codes

--- packages/shop-mobile-expo/App.spec.tsx
+++ packages/shop-mobile-expo/App.spec.tsx
@@ -4,12 +4,12 @@
 
 import 'react-native';
 import React from 'react';
-//import App from './src/App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-
+// Note: test renderer must be required after react-native.
 import { View, Text } from 'react-native';
+
+//  COMPONENTS
+// import App from './src/App';
 
 export default function App() {
 	return (
