f74b0365c6b19a6ed5606caace3f06746ed77917
Neosoulink <neodarksoulink@gmail.com>
2022-02-14 12:48:44 +0100

    refactor: update user store

--- packages/shop-mobile-expo/src/store/features/user/index.ts
+++ packages/shop-mobile-expo/src/store/features/user/index.ts
@@ -1,6 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-//import AsyncStorage from 'async';
+// import AsyncStorage from 'async';
 
 // TYPES
 import type { RootState } from '../../index';
@@ -8,63 +8,24 @@ import type { RootState } from '../../index';
 type UserState = any;
 
 const initialState: UserState = {
-	get_token: null,
-	get_inviteRequestId: null,
-	get_userId: null,
-	get_deviceId: null,
-	get_orderId: null,
-	get_language: null,
-	get_orderWarehouseId: null,
-	get_inviteSystem: null,
-	get_registrationSystem: null,
-	get_buyProduct: null,
-	get_backToDetails: null,
-	get_warehouseId: null,
-	get_maintenanceMode: null,
-	get_deliveryType: null,
-	get_startOrderDate: null,
-	get_endOrderTime: null,
-	get_inviteAddress: null,
-	get_serverConnection: null,
-	get_inStore: null,
-	get_shoppingCartData: null,
-	isLogged: false,
+	data: null,
 };
 
 export const navigationSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-		set_token: () => {},
-		set_inviteRequestId: () => {},
-		set_userId: () => {},
-		set_deviceId: () => {},
-		set_orderId: () => {},
-		set_language: () => {},
-		set_orderWarehouseId: () => {},
-		set_inviteSystem: () => {},
-		set_registrationSystem: () => {},
-		set_buyProduct: () => {},
-		set_backToDetails: () => {},
-		set_warehouseId: () => {},
-		set_maintenanceMode: () => {},
-		set_deliveryType: () => {},
-		set_startOrderDate: () => {},
-		set_endOrderTime: () => {},
-		set_inviteAddress: () => {},
-		set_serverConnection: () => {},
-		set_inStore: () => {},
-		set_shoppingCartData: () => {},
-		clearInStore: () => {},
-		clearMaintenanceMode: () => {},
-		clear: () => {},
+		setData: (state, payload: PayloadAction<any>) => {
+			state.data = payload;
+		},
 	},
 });
 
 // ACTIONS
-export const userActions = navigationSlice.actions;
+export const setUserData = navigationSlice.actions.setData;
 
 // SELECTORS
-export const userSelectors = (state: RootState) => state.user;
+export const getUserObject = (state: RootState) => state.user;
+export const getUserData = (state: RootState) => state.user.data;
 
 export default navigationSlice.reducer;
