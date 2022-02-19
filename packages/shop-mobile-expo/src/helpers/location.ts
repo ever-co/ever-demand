ab2a7cae24ef0c9b48f65c77aef92e982de0f259
Neosoulink <neodarksoulink@gmail.com>
2022-02-19 21:56:58 +0100

    refactor: add return props and update defs names

--- packages/shop-mobile-expo/src/helpers/location.ts
+++ packages/shop-mobile-expo/src/helpers/location.ts
@@ -1,36 +1,40 @@
 import * as Location from 'expo-location';
 
-export interface FormattedAddressInterface {
+export interface FormattedLocationInterface {
 	locality: string;
 	thoroughfare: string;
 	country: string;
+	latitude: number;
+	longitude: number;
 }
 
 /**
  * This helper function return a formatted address using **expo-location**
  *
  * @param coords An object with **Location.LocationObjectCoord** type
- * @returns FormattedAddressType
+ * @returns FormattedLocationType
  */
-export const getFormattedAddress: (
+export const getFormattedLocation: (
 	coords: Location.LocationObjectCoords,
-) => Promise<FormattedAddressInterface | null> = async (coords) => {
+) => Promise<FormattedLocationInterface | null> = async (coords) => {
 	if (coords) {
-		const LocationGeocodedAddress = await Location.reverseGeocodeAsync({
+		const LocationGeoCoded = await Location.reverseGeocodeAsync({
 			longitude: coords?.longitude,
 			latitude: coords?.latitude,
 		});
 
-		console.log(LocationGeocodedAddress);
-		if (LocationGeocodedAddress.length) {
-			const firstLocationAddress = LocationGeocodedAddress[0];
-			const formattedAddress: FormattedAddressInterface = {
-				locality: firstLocationAddress.region as string,
-				country: firstLocationAddress.country as string,
-				thoroughfare: firstLocationAddress.street as string,
+		console.log(LocationGeoCoded);
+		if (LocationGeoCoded.length) {
+			const firstLocationGeoCoded = LocationGeoCoded[0];
+			const formattedLocation: FormattedLocationInterface = {
+				locality: firstLocationGeoCoded.region as string,
+				country: firstLocationGeoCoded.country as string,
+				thoroughfare: firstLocationGeoCoded.street as string,
+				latitude: coords?.longitude,
+				longitude: coords?.longitude,
 			};
 
-			return formattedAddress;
+			return formattedLocation;
 		}
 	}
 
