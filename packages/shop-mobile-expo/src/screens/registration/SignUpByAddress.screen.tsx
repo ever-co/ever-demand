eb49ea2b87305100388383daa7bcdbf5960a904e
Neosoulink <neodarksoulink@gmail.com>
2022-02-19 19:11:08 +0100

    refactor: design & add some logic in SignUpByAddress screen

--- packages/shop-mobile-expo/src/screens/registration/SignUpByAddress.screen.tsx
+++ packages/shop-mobile-expo/src/screens/registration/SignUpByAddress.screen.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import {
 	View,
 	StyleSheet,
+	ScrollView,
 	Alert,
+	TouchableOpacity,
 	// Button as NativeBtn,
 	// Text as NativeTxt,
 } from 'react-native';
-import { ActivityIndicator, TextInput } from 'react-native-paper';
+import {
+	ActivityIndicator,
+	TextInput,
+	Button,
+	HelperText,
+	Checkbox,
+} from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import * as Location from 'expo-location';
 import { showMessage } from 'react-native-flash-message';
+import { validate } from 'validate.js';
 
 // CONSTANTS
 // import GROUPS from '../../router/groups.routes';
@@ -28,6 +37,9 @@ import {
 // COMPONENTS
 import { FocusAwareStatusBar, PaperText } from '../../components/Common';
 
+//  CONSTANTS
+import { REQUIRE_PRESENCE } from '../../constants/rules.validate';
+
 // STYLES
 import {
 	GLOBAL_STYLE as GS,
@@ -35,32 +47,43 @@ import {
 	CONSTANT_COLOR as CC,
 } from '../../assets/ts/styles';
 
-// TYPES
-export interface FormInterface {
-	firstName: string;
-	lastName: string;
-	house: string;
-	apartment: string;
-}
+// TYPE
+export type FormInputNameType =
+	| 'firstName'
+	| 'lastName'
+	| 'house'
+	| 'apartment';
+export type FormType = {
+	[name in FormInputNameType]: string;
+};
+export type FormErrorsType =
+	| {
+			[name in FormInputNameType]: string[] | undefined;
+			// tslint:disable-next-line: indent
+	  }
+	| { [name: string]: string[] | undefined };
 
 const SignUpByAddressScreen = () => {
 	// SELECTORS
-	const currentLanguage = useAppSelector(getLanguage);
+	const CURRENT_LANGUAGE = useAppSelector(getLanguage);
 
 	// ACTIONS
-	const setNavigationGroup = useAppDispatch();
+	const SET_NAVIGATION_GROUP = useAppDispatch();
 
-	// NAVIGATIOn
-	const navigation = useNavigation();
+	// NAVIGATION
+	const NAVIGATION = useNavigation();
 
 	// STATES
 	const [warningDialog, setWarningDialog] = React.useState<boolean>(false);
-	const [form, setForm] = React.useState<FormInterface>({
+	const [form, setForm] = React.useState<FormType>({
 		firstName: '',
 		lastName: '',
 		house: '',
 		apartment: '',
 	});
+	const [formApartmentCheckbox, setFormApartmentCheckbox] =
+		React.useState<boolean>(true);
+	const [formErrors, setFormErrors] = React.useState<FormErrorsType>({});
 	const [loading, setLoading] = React.useState<boolean>(true);
 	const [canGoBack, setCanGoBack] = React.useState<boolean>(false);
 	const [, /* preventBackCallBack */ setPreventBackCallBack] = React.useState<
@@ -70,12 +93,17 @@ const SignUpByAddressScreen = () => {
 		React.useState<Location.LocationObject | null>(null);
 	const [formattedAddress, setFormattedAddress] =
 		React.useState<FormattedAddressInterface | null>(null);
-	const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
 
 	// DATA
 	const STYLES = StyleSheet.create({
+		screen: {
+			...GS.screen,
+			...GS.bgSuccess,
+			overflow: 'hidden',
+		},
 		container: {
 			...GS.screen,
+			...GS.centered,
 			...GS.bgTransparent,
 			...GS.px5,
 			...GS.pb5,
@@ -83,8 +111,10 @@ const SignUpByAddressScreen = () => {
 		},
 		section1: {
 			...GS.centered,
-			...GS.py2,
-			flex: 3,
+			...GS.py5,
+			...GS.my5,
+			height: CS.FONT_SIZE_LG * 1.8 * 5,
+			marginTop: CS.FONT_SIZE_LG * 1.8 * 3,
 		},
 		section1Title: {
 			...GS.txtCenter,
@@ -98,16 +128,58 @@ const SignUpByAddressScreen = () => {
 			fontSize: CS.FONT_SIZE_MD,
 			opacity: 0.6,
 		},
-		section2: { ...GS.py2, flex: 2, alignItems: 'center' },
+		section2: { ...GS.py2, ...GS.w100, alignItems: 'center' },
 		section2Title: {
 			...GS.txtCenter,
 			...GS.mb5,
 			...GS.FF_NunitoBold,
 			fontSize: CS.FONT_SIZE_SM * 2,
 		},
-		formContainer: {},
-		formInput: {},
+		formContainer: {
+			...GS.w100,
+		},
+		formInputContainer: {},
+		formInputContainerRow: { flex: 1 },
+		formInput: { ...GS.bgTransparent, ...GS.mb0, textAlign: 'center' },
+		formInputDisabled: { opacity: 0.4 },
+		formBtn: { ...GS.mb2 },
+		formBtnLabel: {
+			...GS.py1,
+			...GS.txtCapitalize,
+			color: CC.light,
+			fontSize: CS.FONT_SIZE + 3,
+		},
+		formSubmitBtn: { ...GS.bgSecondary },
+		formSkipBtn: { ...GS.bgLight },
 	});
+
+	const VALIDATION_CONSTRAINT = {
+		firstName: REQUIRE_PRESENCE,
+		lastName: REQUIRE_PRESENCE,
+		house: REQUIRE_PRESENCE,
+		apartment: REQUIRE_PRESENCE,
+	};
+
+	// FUNCTIONS
+	const onSubmitForm = () => {
+		setFormErrors({});
+
+		const FORMATTED_FORM = {
+			...form,
+			...formattedAddress,
+		};
+
+		const VALIDATION_RESULT = validate(
+			FORMATTED_FORM,
+			VALIDATION_CONSTRAINT,
+		);
+
+		if (VALIDATION_RESULT) {
+			return setFormErrors(VALIDATION_RESULT);
+		}
+
+		setLoading(true);
+	};
 
 	// EFFECTS
 	React.useEffect(() => {
@@ -117,13 +189,11 @@ const SignUpByAddressScreen = () => {
 				await Location.requestForegroundPermissionsAsync();
 
 			if (status !== 'granted') {
-				const _errorMessage =
-					'Permission to access location was denied';
-				setErrorMsg(_errorMessage);
-				showMessage({ message: _errorMessage });
+				const ERROR_MSG = 'Permission to access location was denied';
+				showMessage({ message: ERROR_MSG });
 				setCanGoBack(true);
 				setTimeout(() => {
-					navigation.goBack();
+					NAVIGATION.goBack();
 				}, 100);
 				return;
 			}
@@ -137,24 +207,23 @@ const SignUpByAddressScreen = () => {
 			setFormattedAddress(FORMATTED_ADDRESS);
 			setLoading(false);
 		})();
-	}, [navigation]);
+	}, [NAVIGATION]);
 
 	React.useEffect(() => {
 		// setCanGoBack(true);
-		// setNavigationGroup(setGroup(GROUPS.APP));
-	}, [setNavigationGroup]);
+		// SET_NAVIGATION_GROUP(setGroup(GROUPS.APP));
+	}, [SET_NAVIGATION_GROUP]);
 
 	React.useEffect(() => {
 		console.log(
 			'\nLocation error ===> ',
 			currentPosition,
-			errorMsg,
 			formattedAddress,
 		);
-	}, [currentPosition, errorMsg, formattedAddress]);
+	}, [currentPosition, formattedAddress]);
 
 	React.useEffect(() => {
-		navigation.addListener('beforeRemove', (e) => {
+		NAVIGATION.addListener('beforeRemove', (e) => {
 			if (canGoBack) {
 				return;
 			}
@@ -168,7 +237,7 @@ const SignUpByAddressScreen = () => {
 				return () => {
 					setCanGoBack(true);
 					setWarningDialog(false);
-					navigation.dispatch(e.data.action);
+					NAVIGATION.dispatch(e.data.action);
 				};
 			});
 
@@ -182,18 +251,18 @@ const SignUpByAddressScreen = () => {
 						style: 'destructive',
 						onPress: () => {
 							setCanGoBack(true);
-							navigation.dispatch(e.data.action);
+							NAVIGATION.dispatch(e.data.action);
 						},
 					},
 				],
 			);
 		});
 
-		return () => navigation.removeListener('beforeRemove', () => null);
-	}, [navigation, canGoBack, warningDialog]);
+		return () => NAVIGATION.removeListener('beforeRemove', () => null);
+	}, [NAVIGATION, canGoBack, warningDialog]);
 
 	return (
-		<View style={{ ...GS.screen }}>
+		<ScrollView style={{ ...GS.screenStatic }}>
 			<FocusAwareStatusBar
 				translucent={false}
 				backgroundColor={CC.primary}
@@ -205,87 +274,211 @@ const SignUpByAddressScreen = () => {
 				{/* section1 */}
 				<View style={STYLES.section1}>
 					<PaperText style={STYLES.section1Title}>
-						{currentLanguage.INVITE_VIEW.YOUR_ADDRESS}
+						{CURRENT_LANGUAGE.INVITE_VIEW.YOUR_ADDRESS}
 					</PaperText>
 
 					<PaperText style={STYLES.section1SubTitle}>
-						{currentLanguage.INVITE_VIEW.LAUNCH_NOTIFICATION}
+						{CURRENT_LANGUAGE.INVITE_VIEW.LAUNCH_NOTIFICATION}
 					</PaperText>
 				</View>
 
 				{/* section2 */}
 				<View style={STYLES.section2}>
-					<PaperText style={STYLES.section2Title}>
-						{currentLanguage.INVITE_VIEW.DETECTING_LOCATION}
-					</PaperText>
-
 					{loading ? (
-						<ActivityIndicator
-							color={CC.light}
-							style={{ ...GS.mt5 }}
-						/>
+						<>
+							<PaperText style={STYLES.section2Title}>
+								{
+									CURRENT_LANGUAGE.INVITE_VIEW
+										.DETECTING_LOCATION
+								}
+							</PaperText>
+							<ActivityIndicator
+								color={CC.light}
+								style={{ ...GS.mt5 }}
+							/>
+						</>
 					) : (
 						<View style={STYLES.formContainer}>
-							<TextInput
-								style={STYLES.formInput}
-								autoComplete={true}
-								placeholder='First name'
-								value={form.firstName}
-								onChangeText={(text) =>
-									setForm((prevForm) => ({
-										...prevForm,
-										firstName: text,
-									}))
-								}
-							/>
-
-							<TextInput
-								style={STYLES.formInput}
-								autoComplete={true}
-								placeholder='Last name'
-								value={form.lastName}
-								onChangeText={(text) =>
-									setForm((prevForm) => ({
-										...prevForm,
-										lastName: text,
-									}))
-								}
-							/>
-
-							<View style={{ ...GS.inlineItems }}>
+							<View style={STYLES.formInputContainer}>
 								<TextInput
-									style={{ ...STYLES.formInput, ...GS.mr2 }}
-									autoComplete={true}
-									placeholder='House'
-									value={form.house}
-									onChangeText={(text) =>
-										setForm((prevForm) => ({
-											...prevForm,
-											house: text,
-										}))
-									}
-								/>
-
-								<TextInput
-									style={STYLES.formInput}
-									autoComplete={true}
-									placeholder='Apartment'
 									value={form.firstName}
+									placeholder='First name'
+									autoComplete='name'
+									textContentType='name'
+									error={!!formErrors.firstName}
+									style={STYLES.formInput}
+									mode='outlined'
 									onChangeText={(text) =>
 										setForm((prevForm) => ({
 											...prevForm,
-											apartment: text,
+											firstName: text,
 										}))
 									}
 								/>
+
+								<HelperText
+									visible={!!formErrors?.firstName}
+									type='error'>
+									{formErrors?.firstName
+										? formErrors.firstName[0]
+										: ''}
+								</HelperText>
 							</View>
+
+							<View style={STYLES.formInputContainer}>
+								<TextInput
+									value={form.lastName}
+									placeholder='Last name'
+									autoComplete='name-family'
+									textContentType='familyName'
+									keyboardType='name-phone-pad'
+									style={STYLES.formInput}
+									mode='outlined'
+									onChangeText={(text) =>
+										setForm((prevForm) => ({
+											...prevForm,
+											lastName: text,
+										}))
+									}
+								/>
+								<HelperText
+									visible={!!formErrors?.lastName}
+									type='error'>
+									{formErrors.lastName
+										? formErrors.lastName[0]
+										: ''}
+								</HelperText>
+							</View>
+
+							<View style={{ ...GS.row }}>
+								<View
+									style={{
+										...STYLES.formInputContainer,
+										...STYLES.formInputContainerRow,
+									}}>
+									<TextInput
+										value={form.house}
+										placeholder='House'
+										keyboardType='default'
+										style={{
+											...STYLES.formInput,
+											...GS.mr2,
+										}}
+										mode='outlined'
+										onChangeText={(text) =>
+											setForm((prevForm) => ({
+												...prevForm,
+												house: text,
+											}))
+										}
+									/>
+									<HelperText
+										visible={!!formErrors?.house}
+										type='error'>
+										{formErrors.house
+											? formErrors.house[0]
+											: ''}
+									</HelperText>
+								</View>
+
+								<View
+									style={{
+										...STYLES.formInputContainer,
+										...STYLES.formInputContainerRow,
+									}}>
+									<TextInput
+										value={
+											formApartmentCheckbox
+												? form.apartment
+												: ''
+										}
+										disabled={!formApartmentCheckbox}
+										editable={formApartmentCheckbox}
+										placeholder={CURRENT_LANGUAGE.APARTMENT}
+										keyboardType='default'
+										style={{
+											...STYLES.formInput,
+											...(!formApartmentCheckbox
+												? STYLES.formInputDisabled
+												: {}),
+										}}
+										theme={{
+											colors: { primary: CC.secondary },
+										}}
+										mode='outlined'
+										onChangeText={(text) =>
+											setForm((prevForm) => ({
+												...prevForm,
+												apartment: text,
+											}))
+										}
+									/>
+
+									<TouchableOpacity
+										onPress={() =>
+											setFormApartmentCheckbox(
+												!formApartmentCheckbox,
+											)
+										}
+										style={GS.justifyContentBetween}>
+										<PaperText>
+											{CURRENT_LANGUAGE.APARTMENT}
+										</PaperText>
+
+										<Checkbox.Android
+											status={
+												formApartmentCheckbox
+													? 'checked'
+													: 'unchecked'
+											}
+											onPress={() =>
+												setFormApartmentCheckbox(
+													!formApartmentCheckbox,
+												)
+											}
+										/>
+									</TouchableOpacity>
+
+									<HelperText
+										visible={!!formErrors?.apartment}
+										type='error'>
+										{formErrors.apartment
+											? formErrors.apartment[0]
+											: ''}
+									</HelperText>
+								</View>
+							</View>
+
+							<Button
+								onPress={onSubmitForm}
+								uppercase={false}
+								style={{
+									...STYLES.formBtn,
+									...STYLES.formSubmitBtn,
+								}}
+								labelStyle={STYLES.formBtnLabel}
+								theme={{ colors: { primary: CC.primary } }}>
+								Submit
+							</Button>
+
+							<Button
+								uppercase={false}
+								style={{
+									...STYLES.formBtn,
+									...STYLES.formSkipBtn,
+								}}
+								labelStyle={{
+									...STYLES.formBtnLabel,
+									color: CC.dark,
+								}}>
+								Skip
+							</Button>
 						</View>
 					)}
 				</View>
-			</View>
 
-			{/* TODO: find how to use custom alert (disable due to slowing virtual device) */}
-			{/* {warningDialog && (
+				{/* TODO: find how to use a custom alert (disable due to slowing virtual device) */}
+				{/* {warningDialog && (
 				<View style={{ ...GS.overlay, ...GS.centered, ...GS.p5 }}>
 					<View
 						style={{
@@ -320,7 +513,8 @@ const SignUpByAddressScreen = () => {
 					</View>
 				</View>
 			)} */}
-		</View>
+			</View>
+		</ScrollView>
 	);
 };
 
