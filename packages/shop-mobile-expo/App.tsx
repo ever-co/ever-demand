18a05bb93bf521ce6c9f4d40148662fa25f487e9
Neosoulink <neodarksoulink@gmail.com>
2022-02-22 14:12:48 +0100

    refactor: minnor

--- packages/shop-mobile-expo/App.tsx
+++ packages/shop-mobile-expo/App.tsx
@@ -1,9 +1,17 @@
 import { registerRootComponent } from 'expo';
-import { LogBox } from 'react-native';
+import { LogBox, View, Text } from 'react-native';
 import ENV from './src/environments/environment';
 
 // APP COMPONENT
-import App from './src/App';
+// import App from './src/App';
+
+const App = () => {
+	return (
+		<View>
+			<Text>H</Text>
+		</View>
+	);
+};
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
