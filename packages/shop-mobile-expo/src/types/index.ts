ea5642ce555961ae130e6289c335bcf54651a2b8
Neosoulink <neodarksoulink@gmail.com>
2022-04-11 17:44:07 +0100

    feat: add user interface

--- packages/shop-mobile-expo/src/types/index.ts
+++ packages/shop-mobile-expo/src/types/index.ts
@@ -14,3 +14,28 @@ export interface ScalarsInterface {
 	Any: any;
 	Void: any;
 }
+
+export interface UserInterface {
+	__typename: string;
+	apartment: string | number;
+	code: string | number;
+	geoLocation: {
+		__typename: string;
+		city: string;
+		coordinates: {
+			__typename: string;
+			lat: number;
+			lng: number;
+		};
+		countryId: number;
+		countryName: string;
+		createdAt: string;
+		house: 1;
+		id: string;
+		notes: string | null;
+		postcode: string | null;
+		streetAddress: string;
+		updatedAt: string;
+	};
+	id: string;
+}
