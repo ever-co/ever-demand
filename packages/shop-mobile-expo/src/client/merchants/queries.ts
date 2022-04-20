5b558566d0634e7e626fd3a617b6a72a72ca133a
Neosoulink <neodarksoulink@gmail.com>
2022-04-20 19:59:11 +0100

    feat: add query to get merchants products

--- packages/shop-mobile-expo/src/client/merchants/queries.ts
+++ packages/shop-mobile-expo/src/client/merchants/queries.ts
@@ -20,3 +20,57 @@ export const GET_MERCHANTS_QUERY: TypedDocumentNode = gql`
 		}
 	}
 `;
+
+export const GET_STORED_PRODUCT: TypedDocumentNode = gql`
+	query GetStoreProducts($storeId: String!, $fullProducts: Boolean!) {
+		getStoreProducts(storeId: $storeId, fullProducts: $fullProducts) {
+			id
+			price
+			count
+			isTakeaway
+			isProductAvailable
+			isDeliveryRequired
+			isCarrierRequired
+			isManufacturing
+			soldCount
+			product {
+				id
+				title {
+					locale
+					value
+				}
+				description {
+					locale
+					value
+				}
+				images {
+					url
+					locale
+					width
+					height
+					orientation
+				}
+				_id
+				descriptionHTML {
+					locale
+					value
+				}
+				_createdAt
+				_updatedAt
+				detailsHTML {
+					value
+					locale
+				}
+				categories
+				details {
+					value
+					locale
+				}
+			}
+			_id
+			initialPrice
+			deliveryTimeMin
+			deliveryTimeMax
+		}
+	}
+`;
