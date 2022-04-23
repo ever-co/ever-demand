aa6ce7186b132f0791c1a9c2f7f2b6aa60e45a13
Neosoulink <neodarksoulink@gmail.com>
2022-04-23 15:25:19 +0100

    refactor: renamed registration screens folder to authentication && add screens

---
+++ packages/shop-mobile-expo/src/screens/authentication/SignIn.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export interface Props {}
+
+const SignInScreen: React.FC<Props> = ({}) => (
+	<View>
+		<Text>SignInScreen</Text>
+	</View>
+);
+
+export default SignInScreen;
---
+++ packages/shop-mobile-expo/src/screens/authentication/SignUp.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export interface Props {}
+
+const SignUpScreen: React.FC<Props> = () => (
+	<View>
+		<Text>SignUpScreen</Text>
+	</View>
+);
+
+export default SignUpScreen;
